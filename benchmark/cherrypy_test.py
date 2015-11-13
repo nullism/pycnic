18e45b70c19f6b04ceaaa75edd39c7c7843f8098
Aliaksei Palkanau <lifusexitus@gmail.com>
2015-11-14 02:47:11 +0300

    Resolve PEP8 violations

--- benchmark/cherrypy_test.py
+++ benchmark/cherrypy_test.py
@@ -1,11 +1,13 @@
 import cherrypy
 
+
 class Root(object):
-    
+
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def json(self):
-        return {"message":"Hello, world!"}
+        return {"message": "Hello, world!"}
+
 
 app = cherrypy.tree.mount(Root())
 cherrypy.log.screen = False
